87035b6536bccb37651445f880ffa7fd5ede95fb
dmitriy a. drachev <dmitriy.a.drachev@gmail.com>
2020-09-21 19:58:42 +0300

    refactoring

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,9 @@
-const About = () => <h2>About page</h2>
+import Link from 'next/link'
 
+const About = () => (
+  <div>
+    <h2>About page</h2>
+    <Link href="/">Home</Link>
+  </div>
+)
 export default About
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
-const Index = () => <h2>Hello from NEXTJS</h2>
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <h2>Hello from NEXTJS</h2>
+    <Link href="/about">About</Link>
+  </div>
+)
 
 export default Index
